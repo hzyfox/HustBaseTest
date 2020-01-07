86ad05b08780397b86105806ad7851c5aa2ad35e
LargerPanda <huayusheng0912@gmail.com>
2020-01-07 19:24:04 +0800

    Test 3 function

--- Test.cpp
+++ Test.cpp
@@ -14,11 +14,15 @@ using namespace std;
 
 void outBin(int x);
 void TestCreatFile();
+void TestOpenFile();
+void TestInsertRec();
 void printlnMsg(RC tmp);
 int main(void)
 {
 
 	TestCreatFile();
+	TestOpenFile();
+	TestInsertRec();
 	return 0;
 }
 
@@ -29,24 +33,28 @@ void TestCreatFile() {
 	}
 	int recordSize = 12;
 	RC tmp = RM_CreateFile(fileName, 12);
-	printlnMsg(tmp);
+	assert(tmp==SUCCESS);
 	int fd = _open(fileName, O_RDONLY | _O_BINARY);
 	Page page0, page1;
 
 	_read(fd, &page0, sizeof(Page));
-	char* bitmap = page0.pData + (int)PF_FILESUBHDR_SIZE;
-	PF_FileSubHeader* fileSubHeader = (PF_FileSubHeader*)page0.pData;
-	
-
 	_read(fd, &page1, sizeof(Page));
 	_close(fd);
+
+
+	char* bitmap = page0.pData + (int)PF_FILESUBHDR_SIZE;
+	PF_FileSubHeader* fileSubHeader = (PF_FileSubHeader*)page0.pData;
+	assert(fileSubHeader->nAllocatedPages == 2);
+	assert(fileSubHeader->pageCount == 1);
+	assert((int)bitmap[0] == 0x3);
+	assert(page0.pageNum == 0);
 
 	char* recBitmap = page1.pData + RM_FILESUBHDR_SIZE;
 	RM_FileSubHeader* rmFileSubHeader = (RM_FileSubHeader*)page1.pData;
 	assert(page1.pageNum == 1);
-	printf("recordSize: %d nRecords: %d firstRecordOffset: %d  recordsPerPage: %d \n",
+	/*printf("recordSize: %d nRecords: %d firstRecordOffset: %d  recordsPerPage: %d \n",
 		rmFileSubHeader->recordSize, rmFileSubHeader->nRecords,
-		rmFileSubHeader->firstRecordOffset, rmFileSubHeader->recordsPerPage);
+		rmFileSubHeader->firstRecordOffset, rmFileSubHeader->recordsPerPage);*/
 	assert(rmFileSubHeader->recordSize == 12);
 	assert(rmFileSubHeader->nRecords == 0);
 	assert(rmFileSubHeader->firstRecordOffset == (int)ceil((float)PF_PAGE_SIZE
@@ -54,14 +62,108 @@ void TestCreatFile() {
 	assert(rmFileSubHeader->recordsPerPage == (PF_PAGE_SIZE - rmFileSubHeader->firstRecordOffset)
 		/ recordSize);
 	assert((int)recBitmap[0] == 0x3);
+	std::cout << "TestCreatFile:" << "SUCCESS" << std::endl;
+	
 }
 
 
 void TestOpenFile() {
+	RM_FileHandle rmFileHandle;
+	char fileName[] = "123.txt";
+	if (_access(fileName, 0) != -1) {
+		remove(fileName);
+	}
+	RM_CreateFile(fileName, 12);
+	RC tmp = RM_OpenFile(fileName, &rmFileHandle);
+	assert(tmp==SUCCESS);
+	
 
+	PF_FileSubHeader* fileSubHeader = (PF_FileSubHeader*)rmFileHandle.pFileHandler->pFileSubHeader;
+	Frame* pHdrFrame = rmFileHandle.pFileHandler->pHdrFrame;
+	char* bitmap = rmFileHandle.pFileHandler->pBitmap;
+	//std::cout << "fileSubHeader->nAllocatedPage: " << fileSubHeader->nAllocatedPages << std::endl;
+	assert(fileSubHeader->nAllocatedPages == 2);
+	assert(fileSubHeader->pageCount == 1);
+	assert((int)bitmap[0] == 0x3);
+	assert(rmFileHandle.pFileHandler->pHdrPage->pageNum == 0);
+	assert(pHdrFrame->bDirty == false && pHdrFrame->pinCount == 1);
+	
 
+	RM_FileSubHeader* rmFileSubHeader = rmFileHandle.pRecordFileSubHeader;
+	Frame* recFrame = rmFileHandle.pRecFrame;
+	char* recBitmap = rmFileHandle.pRecordBitmap;
+	int recordSize = rmFileSubHeader->recordSize;
+	assert(rmFileHandle.bOpen);
+	/*printf("recordSize: %d nRecords: %d firstRecordOffset: %d  recordsPerPage: %d \n",
+		rmFileSubHeader->recordSize, rmFileSubHeader->nRecords,
+		rmFileSubHeader->firstRecordOffset, rmFileSubHeader->recordsPerPage);*/
+	assert(rmFileSubHeader->recordSize == 12);
+	assert(rmFileSubHeader->nRecords == 0);
+	assert(rmFileSubHeader->firstRecordOffset == (int)ceil((float)PF_PAGE_SIZE
+		/ (float)(8 * recordSize + 1)));
+	assert(rmFileSubHeader->recordsPerPage == (PF_PAGE_SIZE - rmFileSubHeader->firstRecordOffset)
+		/ recordSize);
+	assert((int)recBitmap[0] == 0x3);
+	assert(rmFileHandle.pRecPage->pageNum == 1);
+	assert(recFrame->bDirty == false && recFrame->pinCount == 1);
+	
+	RM_CloseFile(&rmFileHandle);
+	std::cout << "TestOpenFile:" << "SUCCESS" << std::endl;
 
 }
+
+void TestInsertRec() {
+	char pData[12];
+	*((int*)pData) = 5;
+	*(((int*)pData)+1) = 2;
+	*(((int*)pData) + 1) = 1;
+
+	RID rid;
+	RM_FileHandle rmFileHandle;
+	char fileName[] = "123.txt";
+	if (_access(fileName, 0) != -1) {
+		remove(fileName);
+	}
+	RM_CreateFile(fileName, 12);
+	RC tmp = RM_OpenFile(fileName, &rmFileHandle);
+	assert(tmp==SUCCESS);
+
+	tmp = InsertRec(&rmFileHandle, pData, &rid);
+	assert(tmp==SUCCESS);
+
+	RM_FileSubHeader* rmFileSubHeader = rmFileHandle.pRecordFileSubHeader;
+	Frame* recFrame = rmFileHandle.pRecFrame;
+	char* recBitmap = rmFileHandle.pRecordBitmap;
+	int recordSize = rmFileSubHeader->recordSize;
+	
+
+	assert(rid.bValid == true);
+	assert(rid.pageNum == 2);
+	assert(rid.slotNum == 0);
+	assert(rmFileSubHeader->nRecords == 1);
+	assert((int)recBitmap[0] == 0x3);
+
+
+	tmp = InsertRec(&rmFileHandle, pData, &rid);
+	assert(rid.slotNum == 1);
+	assert(rmFileSubHeader->nRecords == 2);
+	RM_CloseFile(&rmFileHandle);
+
+	tmp = RM_OpenFile(fileName, &rmFileHandle);
+	assert(tmp==SUCCESS);
+
+	tmp = InsertRec(&rmFileHandle, pData, &rid);
+	assert(tmp==SUCCESS);
+
+	assert(rid.bValid == true);
+	assert(rid.pageNum == 2);
+	assert(rid.slotNum == 2);
+	assert(rmFileSubHeader->nRecords == 3);
+
+	std::cout << "TestInsertRec:" << "SUCCESS" << std::endl;
+}
+
+
 
 void outBin(int x) {
 	std::cout << bitset<sizeof(int) * 8>(x) << std::endl;
